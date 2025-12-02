1c0871fb5d8281ee71ab5189494a3180cf571dcc
Ronaldo <ronaldo.rosa@bento.ifrs.edu.br>
2025-12-02 05:07:37 -0300

    Adiciona segundo produto ao endpoint /products

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
 app.get('/products', (req, res) => {
-    res.json([{ id: 1, name: 'Produto Exemplo' }]);
+    res.json([
+        { id: 1, name: 'Produto Exemplo' },
+        { id: 2, name: 'Outro Produto' }
+    ]);
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`🚀 API Loja disponível em http://localhost:${PORT}/products`);
 });
